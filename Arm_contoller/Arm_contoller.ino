2885f6759b2209f64f0f001f5a4616fe5ee1f259
Andres Becker <abecker.esfm@gmail.com>
2020-09-18 08:49:49 +0200

    Code added to control the 5 servos and the 2 Joysticks and pince

--- Arm_contoller/Arm_contoller.ino
+++ Arm_contoller/Arm_contoller.ino
@@ -11,39 +11,58 @@
 //Declare Servo pin in Arduiono (Out signal)
 int xServoPin = 3;
 int yServoPin = 5;
-int pincerServoPin = 6;
+int xServoPin2 = 6;
+int yServoPin2 = 9;
+int pincerServoPin = 10;
+
 //Create Servo object
 Servo xServo;
 Servo yServo;
+Servo xServo2;
+Servo yServo2;
 Servo pincerServo;
 
-float JoystickRate = 10.0;
+// Constants
+float JoystickRate = 2;
+int AngLimInf = 0;
+int AngLimSup = 170;
+
+
 float xRate = 0;
 float xSum = 0;
 float yRate = 0;
 float ySum = 0;
 
-int AngLimInf = 0;
-int AngLimSup = 170;
+float xRate2 = 0;
+float xSum2 = 0;
+float yRate2 = 0;
+float ySum2 = 0;
 
 // Set input for Joystick
 int xPin = A0;
 int yPin = A1;
-int buttonPin = 2;
+//int buttonPin = 2;
+int xPin2 = A2;
+int yPin2 = A3;
 
 // Set input pin for Variable resistance (Pincer)
-int pincerPin = A2;
+int pincerPin = A4;
 int pincerPos = 0;
-int pincerAng = AngLimSup;
+//int pincerAng = AngLimSup;
+int pincerAng = 90;
 
 // Set status vars
 int xPos = 0;
 int yPos = 0;
-int buttonStat = 0;
+//int buttonStat = 0;
+int xPos2 = 0;
+int yPos2 = 0;
 
 // Status in degrees
-int xAng = 90;
-int yAng = 90;
+int xAng = 130;
+int yAng = 80;
+int xAng2 = 25;
+int yAng2 = 155;
 
 
 void setup() {
@@ -53,7 +72,9 @@ void setup() {
   //set Arduino Pins mode as INPUT
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  //pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(xPin2, INPUT);
+  pinMode(yPin2, INPUT);
 
   //Pincer
   pinMode(pincerPin, INPUT);
@@ -61,20 +82,30 @@ void setup() {
   //Servo setups
   xServo.attach(xServoPin);
   yServo.attach(yServoPin);
+  xServo2.attach(xServoPin2);
+  yServo2.attach(yServoPin2);
   pincerServo.attach(pincerServoPin);
   //Init servo position in 90 degrees
+  xServo2.write(xAng2);
+  yServo2.write(yAng2);
   xServo.write(xAng);
   yServo.write(yAng);
+
   pincerServo.write(pincerAng);
   
 }
 
 void loop() {
+
+  // Read Joysticks input
   xPos = analogRead(xPin);
   yPos = analogRead(yPin);
-  buttonStat = digitalRead(buttonPin);
+  xPos2 = analogRead(xPin2);
+  yPos2 = analogRead(yPin2);
+  //buttonStat = digitalRead(buttonPin);
   pincerPos = analogRead(pincerPin);
 
+  // Process for firs Joystick and servos 1 & 2
   xRate = ((float)xPos - 510.0) / 510.0;
   yRate = ((float)yPos - 510.0) / 510.0;
 
@@ -88,27 +119,69 @@ void loop() {
   }
 
   xAng = xAng + round(xSum);
-  if (xAng < AngLimInf) {
-    xAng = AngLimInf;
+  if (xAng < 10) {
+    xAng = 10;
   }
-  else if (xAng > AngLimSup) {
-    xAng = AngLimSup;
+  else if (xAng > 135) {
+    xAng = 135;
   }
 
   yAng = yAng + round(ySum);
-  if (yAng < AngLimInf) {
-    yAng = AngLimInf;
+  if (yAng < 10) {
+    yAng = 10;
   }
-  else if (yAng > AngLimSup) {
-    yAng = AngLimSup;
+  else if (yAng > 170) {
+    yAng = 170;
+  }
+
+  // Process for second Joystick and servos 3 & 4
+  xRate2 = ((float)xPos2 - 510.0) / 510.0;
+  yRate2 = ((float)yPos2 - 510.0) / 510.0;
+
+  xSum2 = xRate2 * JoystickRate;
+  if ((xSum2 < 1) && (-1 < xSum2)){
+    xSum2 = 0;
+  }
+  ySum2 = yRate2 * JoystickRate;
+  if ((ySum2 < 1) && (-1 < ySum2)){
+    ySum2 = 0;
+  }
+
+  xAng2 = xAng2 - round(xSum2);
+  if (xAng2 < 5) {
+    xAng2 = 5;
+  }
+  else if (xAng2 > 140) {
+    xAng2 = 140;
+  }
+
+  yAng2 = yAng2 - round(ySum2);
+  if (yAng2 < 10) {
+    yAng2 = 10;
+  }
+  else if (yAng2 > 160) {
+    yAng2 = 160;
   }
 
   //Pincer
   pincerAng = round((float)AngLimSup * ((float)pincerPos / 1023.0));
+  if (pincerAng < 40) {
+    pincerAng = 40;
+  }
+  else if (pincerAng > 120) {
+    pincerAng = 120;
+  }
+
+  Serial.print("Pincer: ");
+  Serial.print(pincerAng);
+  Serial.println("");
+  Serial.println("");
   
 
   xServo.write(xAng);
   yServo.write(yAng);
+  xServo2.write(xAng2);
+  yServo2.write(yAng2);
   pincerServo.write(pincerAng);
 
   /*
@@ -132,13 +205,12 @@ void loop() {
   Serial.println("");
   
   Serial.println("");
-  */
+  
   Serial.print("PincerPos: ");
   Serial.println(pincerPos);
   Serial.print("PincerAng: ");
   Serial.println(pincerAng);
   Serial.println("");
-
+*/
   delay(50);
-  //delay(500);
 }
